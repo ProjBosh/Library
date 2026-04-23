316c6934f66891070436a2d71884aec1ef0a78b6
Dmitry <d.boshchenko@gmail.com>
2026-04-23 19:30:54 +0300

    Доработка

--- src/main/java/ru/library/books/BooksViewController.java
+++ src/main/java/ru/library/books/BooksViewController.java
@@ -1,4 +1,23 @@
 package ru.library.books;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
 public class BooksViewController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public String showAllBooks(Model model) {
+        List<Book> booksList = bookRepository.findAll();
+        model.addAttribute("books", booksList);
+        return "books-list"; // Вернет books-list.html
+    }
 }
