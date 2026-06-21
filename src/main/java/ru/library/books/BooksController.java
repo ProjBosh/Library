ba89d1465fa6e5c52080d33f9b0f9e6c40f04585
Dmitry <d.boshchenko@gmail.com>
2026-06-21 13:10:40 +0300

    Убрана аннотация @Autowired

--- src/main/java/ru/library/books/BooksController.java
+++ src/main/java/ru/library/books/BooksController.java
@@ -1,6 +1,5 @@
 package ru.library.books;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +10,11 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BooksController {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+
+    BooksController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @GetMapping
     public List<Book> getAllBooks() {
