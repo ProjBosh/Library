c8eb9d602ef365b97a37f47424a36e1eb922c54c
Dmitry <d.boshchenko@gmail.com>
2026-07-19 12:32:13 +0300

    Изменена аннотация Data на Getter и Setter

--- src/main/java/ru/library/books/Book.java
+++ src/main/java/ru/library/books/Book.java
@@ -1,9 +1,11 @@
 package ru.library.books;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "books")
 public class Book {
